34e4a763f3f176a6dade2011772967bb94ba6ffb
Neil Bost <bost.neil@gmail.com>
2021-09-01 21:43:51 +0000

    Reformatted chapters 1-3.

--- Ch2_Variables.rb
+++ Ch2_Variables.rb
@@ -1,10 +1,9 @@
-puts "1."
+# 1.
 puts "Hi, what's your name?"
 name = gets.chomp
 puts "Hello #{name}, how are you?"
 
-puts
-puts "2."
+# 2.
 puts "How old are you?"
 age = gets.chomp
 puts "In 10 years you will be:"
@@ -16,20 +15,17 @@ puts age.to_i + 30
 puts "In 40 years you will be:"
 puts age.to_i + 40
 
-puts
-puts "3."
+# 3.
 puts "Hi, what's your name?"
 name = gets.chomp
 10.times { puts name }
 
-puts
-puts "4."
+# 4.
 puts "Hi, what is your first name?"
 first_name = gets.chomp
 puts "What is your last name?"
 last_name = gets.chomp
 puts "Your full name is #{first_name} #{last_name}."
 
-puts
-puts "5."
-puts "The first puts '3'. The second puts an error message because the x variable's scope is only inner, not outer, because it was created inside the block."
+# 5.
+# The first puts '3'. The second puts an error message because the x variable's scope is only inner, not outer, because it was created inside the block.
